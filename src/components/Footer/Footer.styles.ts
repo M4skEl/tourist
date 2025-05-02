1edfa9f33b70791f25456338779650d046b1635a
Elisey Siver <sivereliseu7@gmail.com>
2025-05-02 23:32:54 +0300

    добавил логотип фонда

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -2,168 +2,175 @@ import {createStyles} from "@/utils/createStyles";
 import Colors from "@/utils/Colors";
 
 export const styles = () =>
-    createStyles({
-        sectionContainer: {
-            padding: '40px 40px 40px',
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
-            backgroundColor: Colors.green,
-        },
+  createStyles({
+    sectionContainer: {
+      padding: '40px 40px 40px',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      backgroundColor: Colors.green,
+    },
 
-        title: {
-            fontSize: '48px',
-            fontWeight: 'bold',
-            //marginBottom: '40px',
-            lineHeight: '70px',
-            color: Colors.white,
-            '@media (max-width: 600px)': {
-                fontSize: '32px',
-            },
-        },
-        contentContainer: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-            position: 'relative',
+    title: {
+      fontSize: '48px',
+      fontWeight: 'bold',
+      //marginBottom: '40px',
+      lineHeight: '70px',
+      color: Colors.white,
+      '@media (max-width: 600px)': {
+        fontSize: '32px',
+      },
+    },
+    contentContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+      position: 'relative',
 
-            //maxWidth: '1080px',
-            width: '100%',
-            margin: '0 40px',
-            paddingTop: '40px',
-            borderTop:`2px solid ${Colors.gray}`,
-        },
+      //maxWidth: '1080px',
+      width: '100%',
+      margin: '0 40px',
+      paddingTop: '40px',
+      borderTop: `2px solid ${Colors.gray}`,
+    },
 
-        socialsContainer: {
-            display: 'flex',
-            width: '25%',
-            marginTop: '70px',
-            //justifyContent: 'flex-end',
-            alignItems: 'flex-end',
-            //flexDirection: 'column',
-        },
-        socialLink: {
-            marginTop:'70px',
-        },
-        socialLinkImg: {
-            width: '50px',
-            height: '50px',
-            '@media (max-width: 500px)': {
-                width: '40px',
-                height: '40px',
-                minWidth:'40px',
-                minHeight:'40px',
-            },
-        },
-        descriptionContainer: {
-            textAlign: 'center',
-            width: '50%',
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
-            '@media (max-width: 1100px)': {
-                width: '75%',
-            },
-
-
-        },
-        description: {
-            fontSize: '22px',
-            lineHeight: '30px',
-            color: Colors.white,
-            fontWeight: 300,
-            //marginBottom: '20px',
-            //opacity:.8,
-            '@media (max-width: 600px)': {
-                fontSize: '18px',
-            },
-        },
+    socialsContainer: {
+      display: 'flex',
+      width: '25%',
+      marginTop: '70px',
+      //justifyContent: 'flex-end',
+      alignItems: 'flex-end',
+      //flexDirection: 'column',
+    },
+    socialLink: {
+      marginTop: '70px',
+    },
+    socialLinkImg: {
+      width: '50px',
+      height: '50px',
+      '@media (max-width: 500px)': {
+        width: '40px',
+        height: '40px',
+        minWidth: '40px',
+        minHeight: '40px',
+      },
+    },
+    descriptionContainer: {
+      textAlign: 'center',
+      width: '50%',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      '@media (max-width: 1100px)': {
+        width: '75%',
+      },
 
 
+    },
+    description: {
+      fontSize: '22px',
+      lineHeight: '30px',
+      color: Colors.white,
+      fontWeight: 300,
+      //marginBottom: '20px',
+      //opacity:.8,
+      '@media (max-width: 600px)': {
+        fontSize: '18px',
+      },
+    },
 
 
-        firstRow:{
-            display: 'flex',
-            width: '100%',
-            gap:'80px',
-            justifyContent: 'center',
-        },
-        secondRow:{
-            display: 'flex',
-            width: '100%',
-            gap:'40px',
-            justifyContent: 'center',
+    firstRow: {
+      display: 'flex',
+      width: '100%',
+      gap: '80px',
+      justifyContent: 'center',
+    },
+    secondRow: {
+      display: 'flex',
+      width: '100%',
+      gap: '40px',
+      justifyContent: 'center',
 
-        },
-        linksContainer: {
-           // width: '30%',
-            //marginTop: '140px',
-            display: 'flex',
-            flexDirection: 'column',
-            //gap: '10px',
-        },
-        link: {
-            fontSize: '24px',
-            cursor: 'pointer',
-            color: '#5c5c5c',
-            fontWeight: '300',
-            '&:hover': {
-                opacity: .8,
-            }
-        },
-        linkText:{
-            fontWeight: '300',
-            fontSize: '20px',
-            '&:hover': {
-                opacity: .8,
-            }
-        },
-
-
-        GovermentContainer:{
-            marginTop: '150px',
-            '@media (max-width: 768px)': {
-                maxWidth: '100%',
-            },
+    },
+    linksContainer: {
+      // width: '30%',
+      //marginTop: '140px',
+      display: 'flex',
+      flexDirection: 'column',
+      //gap: '10px',
+    },
+    link: {
+      fontSize: '24px',
+      cursor: 'pointer',
+      color: '#5c5c5c',
+      fontWeight: '300',
+      '&:hover': {
+        opacity: .8,
+      }
+    },
+    linkText: {
+      fontWeight: '300',
+      fontSize: '20px',
+      '&:hover': {
+        opacity: .8,
+      }
+    },
 
 
-        },
-        govermentDescription:{
-            marginTop: '40px',
-           // padding: '0 40px',
-            fontSize: '12px',
-            fontWeight: '300',
-            color: Colors.white,
+    GovermentContainer: {
+      marginTop: '150px',
+      '@media (max-width: 768px)': {
+        maxWidth: '100%',
+      },
 
-        },
 
-        contactsContainer:{
-            display: 'flex',
-            gap:'10px',
-            flexDirection: 'column',
-            alignItems: 'center',
-            //marginTop: '70px',
-            //width: '25%',
-            marginTop: '30px',
-            '@media (max-width: 650px)': {
-                gap:'5px',
-            },
-        },
-        contactsRow:{
-            display: 'flex',
-            gap:'50px',
-            '@media (max-width: 650px)': {
-               flexDirection: 'column',
-                gap:'5px',
-                alignItems: 'center',
-            },
-        },
-        contacts:{
-            //width: '25%',
-            textAlign: 'left',
-            color: Colors.white,
-            fontSize: '14px',
-            fontWeight: '300',
-            textWrap:'nowrap',
-        },
-    })
+    },
+    governmentDescription: {
+
+      // padding: '0 40px',
+      fontSize: '12px',
+      fontWeight: '300',
+      color: Colors.white,
+      paddingRight: '100px',
+    },
+    logotype: {
+      width: '100px',
+      objectFit: 'contain',
+    },
+    governmentContainer: {
+      marginTop: '40px',
+      display:'flex',
+      gap:'5px',
+     alignItems: 'center',
+    },
+    contactsContainer: {
+      display: 'flex',
+      gap: '10px',
+      flexDirection: 'column',
+      alignItems: 'center',
+      //marginTop: '70px',
+      //width: '25%',
+      marginTop: '30px',
+      '@media (max-width: 650px)': {
+        gap: '5px',
+      },
+    },
+    contactsRow: {
+      display: 'flex',
+      gap: '50px',
+      '@media (max-width: 650px)': {
+        flexDirection: 'column',
+        gap: '5px',
+        alignItems: 'center',
+      },
+    },
+    contacts: {
+      //width: '25%',
+      textAlign: 'left',
+      color: Colors.white,
+      fontSize: '14px',
+      fontWeight: '300',
+      textWrap: 'nowrap',
+    },
+  })
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,35 +6,38 @@ import {styles} from "@/components/Footer/Footer.styles";
 const Footer = () => {
 
 
-    const {
-        sectionContainer,
-        contentContainer,
-        //socialsContainer,
-        socialLink,
-        socialLinkImg,
-        contactsContainer,
-        contactsRow,
+  const {
+    sectionContainer,
+    contentContainer,
+    //socialsContainer,
+    socialLink,
+    socialLinkImg,
+    contactsContainer,
+    contactsRow,
 
 
-        descriptionContainer,
-        title,
-        description,
+    descriptionContainer,
+    title,
+    description,
 
-        linksContainer,
-        govermentDescription,
-        contacts,
-    } = styles()
+    linksContainer,
+    governmentDescription,
+    contacts,
+    logotype,
+    governmentContainer,
+  } = styles()
 
 
-    return (
-        <Box component={'section'} sx={sectionContainer} id={'contacts'}>
+  return (
+    <Box component={'section'} sx={sectionContainer} id={'contacts'}>
 
-            <Box sx={contentContainer}>
-                <Box component={'a'} sx={socialLink} href={'https://vk.com/retrospektiva_arh/'} target={'_blank'} rel='noopener noreferrer nofollow'>
-                    <Box component={'img'} sx={socialLinkImg} src={'/icons/VK.png'}/>
-                </Box>
+      <Box sx={contentContainer}>
+        <Box component={'a'} sx={socialLink} href={'https://vk.com/retrospektiva_arh/'} target={'_blank'}
+             rel='noopener noreferrer nofollow'>
+          <Box component={'img'} sx={socialLinkImg} src={'/icons/VK.png'}/>
+        </Box>
 
-                {/*<Box sx={socialsContainer}>
+        {/*<Box sx={socialsContainer}>
                     <Box component={'a'} sx={socialLink} href={'https://vk.com/'} rel='noopener noreferrer'>
                         <Box component={'img'} style={socialLinkImg} src={'/icons/VK.png'}/>
                     </Box>
@@ -43,14 +46,14 @@ const Footer = () => {
                     </Box>
                 </Box>*/}
 
-                <Box sx={descriptionContainer}>
-                    <Typography sx={title}>{'РЕТРОСПЕКТИВА'}</Typography>
-                    <Typography sx={description}>
-                        {'ФОТОЭКСКУРСИИ ПО ГОРОДУ'}<br/>
-                        {'НА РЕТРОАВТОМОБИЛЕ'}
-                    </Typography>
-                    <Box sx={linksContainer}>
-                        {/* <Box sx={firstRow}>
+        <Box sx={descriptionContainer}>
+          <Typography sx={title}>{'РЕТРОСПЕКТИВА'}</Typography>
+          <Typography sx={description}>
+            {'ФОТОЭКСКУРСИИ ПО ГОРОДУ'}<br/>
+            {'НА РЕТРОАВТОМОБИЛЕ'}
+          </Typography>
+          <Box sx={linksContainer}>
+            {/* <Box sx={firstRow}>
                             <Link href="/" style={link}>
                                 <Typography component={'span'} sx={linkText}>
                                     {'Почему'}
@@ -79,38 +82,40 @@ const Footer = () => {
                                 </Typography>
                             </Link>
                         </Box>*/}
-
-                        <Typography sx={govermentDescription}>
-                            {'Проект создан при поддержке Федерального государственного бюджетного учреждения "Фонд содействия развитию малых форм предприятий в научно-технической сфере в рамках программы "Студенческий стартап" федерального проекта "Платформа университетского технологического предпринимательства"'}<br/>
-                        </Typography>
-                        <Box sx={contactsContainer}>
-                            <Box sx={contactsRow}>
-                                <Typography component='span' sx={contacts}>
-                                    {'ООО "Ретроспектива"'}</Typography>
-                                <Typography component='span' sx={contacts}>
-                                    {'ИНН 2901317840'} </Typography>
-                                <Typography component='span' sx={contacts}>
-                                    {'г. Архангельск'} </Typography>
-                            </Box>
-                            <Box sx={contactsRow}>
-                                <Typography component='span' sx={contacts}>
-                                    {'Тел.: +7 (921) 293-19-52'}</Typography>
-                                <Typography component='span' sx={contacts}>
-                                    {'E-mail: ArhRetrospektr@yandex.ru'}
-                                </Typography>
-                            </Box>
-                        </Box>
-                    </Box>
-
-
-                </Box>
-                <Box component={'a'} sx={socialLink} href={'https://web.telegram.org/'} rel='noopener noreferrer'>
-                    <Box component={'img'} sx={socialLinkImg} src={'/icons/Telegram.png'}/>
-                </Box>
+            <Box sx={governmentContainer}>
+              <Box component={'img'} sx={logotype} src={'/icons/LogoFond.png'}/>
+              <Typography sx={governmentDescription}>
+                {'Проект создан при поддержке Федерального государственного бюджетного учреждения "Фонд содействия развитию малых форм предприятий в научно-технической сфере в рамках программы "Студенческий стартап" федерального проекта "Платформа университетского технологического предпринимательства"'}<br/>
+              </Typography>
             </Box>
+            <Box sx={contactsContainer}>
+              <Box sx={contactsRow}>
+                <Typography component='span' sx={contacts}>
+                  {'ООО "Ретроспектива"'}</Typography>
+                <Typography component='span' sx={contacts}>
+                  {'ИНН 2901317840'} </Typography>
+                <Typography component='span' sx={contacts}>
+                  {'г. Архангельск'} </Typography>
+              </Box>
+              <Box sx={contactsRow}>
+                <Typography component='span' sx={contacts}>
+                  {'Тел.: +7 (921) 293-19-52'}</Typography>
+                <Typography component='span' sx={contacts}>
+                  {'E-mail: ArhRetrospektr@yandex.ru'}
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+
 
         </Box>
-    )
+        <Box component={'a'} sx={socialLink} href={'https://t.me/retrospektiva_arh_bot'} rel='noopener noreferrer'>
+          <Box component={'img'} sx={socialLinkImg} src={'/icons/Telegram.png'}/>
+        </Box>
+      </Box>
+
+    </Box>
+  )
 }
 
 export default Footer
